012f3a05c8ffda0adc25cbdc6d71194f82b58621
HectorUVG <87456605+HectorUVG@users.noreply.github.com>
2021-09-14 15:40:40 -0600

    Add files via upload

---
+++ Organico.java
@@ -0,0 +1,47 @@
+//*****************************************************************************
+//Imports
+//*****************************************************************************
+
+//*****************************************************************************
+//Clase
+//*****************************************************************************
+
+public class Organico {
+
+//*****************************************************************************
+//variables
+//*****************************************************************************
+    private int frutasVerduras;
+    private int pollo;
+    private int carne;
+    private int cerdos;
+    private int pescado;
+    private int edad;
+    private Persona persona;//el nombre de esta clase puede cambiar dependiendo como le pongan  
+
+//*****************************************************************************
+//Constructores
+//*****************************************************************************
+
+//*****************************************************************************
+//Metodos
+//*****************************************************************************
+    public int conversionFrutasVerduras(int frutasVerduras){
+
+    }
+    public int conversionPollo(int pollo){
+
+    }
+    public int conversionCarne(int carne){
+
+    }
+    public int conversionCerdos(int cerdos){
+
+    }
+    public int conversionPescados(int pescado){
+        
+    }
+
+
+
+}
