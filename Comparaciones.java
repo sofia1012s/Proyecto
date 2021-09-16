788b1ac618ee4b2aa0eacda6d9e0741735f318bb
Sofía Salguero <salguerosofia.1012@gmail.com>
2021-09-16 07:58:28 -0600

    Se crearon métodos

--- Comparaciones.java
+++ Comparaciones.java
@@ -1,2 +1,62 @@
-public class Comparaciones{
+public class Comparaciones {
+
+    private Inorganico inorganico;
+    private Organico organico;
+    private PersonaPromedio personaP;
+
+    public int comparacionBotellas(int botellas) {
+        // botellas = cantidad de botellas en cierta cantidad de tiempo
+        //
+        return botellas;
+    }
+
+    public int comparacionPapel(int papel) {
+
+        return papel;
+    }
+
+    public int comparacionAgua(int tiempo) {
+
+        return tiempo;
+    }
+
+    public int comparacionBolsas(int bolsas) {
+
+        return bolsas;
+    }
+
+    public int comparacionCarro(int km) {
+
+        return km;
+    }
+
+    public int comparacionVerduras(int frutasVerduras) {
+
+        return frutasVerduras;
+    }
+
+    public int comparacionPollo(int pollo) {
+
+        return pollo;
+    }
+
+    public int comparacionCarne(int carne) {
+
+        return carne;
+    }
+
+    public int comparacionCerdo(int cerdo) {
+
+        return cerdo;
+    }
+
+    public int comparacionPescado(int pescado) {
+
+        return pescado;
+    }
+
+    public int comparacionPersonaP(int cantidadPromedio) {
+
+        return cantidadPromedio;
+    }
 }
