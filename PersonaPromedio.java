fa9f886573e0a600ec48a4f274e94c30eddb1106
Jeremy-Mejia <87196731+Jeremy-Mejia@users.noreply.github.com>
2021-09-15 21:35:09 -0600

    Terminación de persona promedio

--- PersonaPromedio.java
+++ PersonaPromedio.java
@@ -8,4 +8,41 @@ class PersonaPromedio {
     private int carnePromedio = 51; //Kilos al año 
     private int cerdoPromedio = 17; //Kilos al año
     private int pescadossPromedio = 20; //Kilos al añox
+
+    public void BotellasPromedio(){
+        System.out.println("Una persona promedio consume " + botellasPromedio + " botellas cada mes");
+    }
+
+    public void PapelPromedio(){
+        System.out.println("Una persona promedio consume " + papelPromedio + " kg de papel en un año");
+    }
+
+    public void AguaPromedio(){
+        System.out.println("Una persona promedio consume " + aguaPromedio + " litros de agua al día");
+    }
+
+    public void BolsasPromedio(){
+        System.out.println("Una persona promedio consume " + bolsasPromedio + " bolsas al año");
+    }
+
+    public void FrutasPromedio(){
+        System.out.println("Una persona promedio consume " + frutasVerdurasPromedio + " gramos de verduras y frutas al día");
+    }
+    
+    public void PolloPromedio(){
+        System.out.println("Una persona promedio consume " + polloPromedio + " libras de pollo al año");
+    }
+
+    public void CarnePromedio(){
+        System.out.println("Una persona promedio consume " + carnePromedio + " kilos de carne al año");
+    }
+
+    public void CerdoPromedio(){
+        System.out.println("Una persona promedio consume " + cerdoPromedio+ " kilos de cerdo al año");
+    }
+
+    public void PescadoPromedio(){
+        System.out.println("Una persona promedio consume " + pescadossPromedio + " kilos de pescado al año");
+    }
+    
 }
