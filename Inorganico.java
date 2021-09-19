5f6ce7acab48ace5268237fdaca7c6d829a82441
HectorUVG <87456605+HectorUVG@users.noreply.github.com>
2021-09-19 15:10:00 -0600

    finalizacion de la primera version de las clases organico e inorganico

--- Inorganico.java
+++ Inorganico.java
@@ -20,7 +20,7 @@ public class Inorganico {
     private int tiempo;
     private int bolsas;
     private int edad;
-    private Persona persona;
+   // private Persona persona;
 
 //*****************************************************************************
 //Constructores
@@ -29,16 +29,40 @@ public class Inorganico {
 //*****************************************************************************
 //Metodos
 //*****************************************************************************
-    public int conversionBotellas(int botellas){
+    public int conversionBotellas(int botellas, int tiempo){
+        /**
+         * Realiza una multiplicación de la cantidad de botellas por semana 
+         * por una cantidad de tiempo específico.
+         */
+
+        int conversion = botellas*tiempo;
+        return conversion;
 
     }
-    public int conversionPapel(int papel){
+    public int conversionPapel(int papel, int tiempo){
+        /**
+         * Realiza una multiplicación de la cantidad de papel por semana 
+         * por una cantidad de tiempo específico.
+         */
+        int conversion = papel*tiempo;
+        return conversion;
 
     }
-    public int conversionAgua(int tiempo){
+    public int conversionAgua(int tiempoBaniandose, int tiempo){
+        /**
+         * Realiza una multiplicación de la cantidad de agua consumida 
+         * por una cantidad de tiempo específico.
+         */
+        int conversion = tiempoBaniandose*tiempo;
+        return conversion;
 
     }
-    public int conversionBolsas(int bolsas){
-        
+    public int conversionBolsas(int bolsas, int tiempo){
+        /**
+         * Realiza una multiplicación de la cantidad de bolsas por semana 
+         * por una cantidad de tiempo específico.
+         */
+        int conversion = bolsas*tiempo;
+        return conversion;
     }
 }
--- Organico.java
+++ Organico.java
@@ -17,7 +17,7 @@ public class Organico {
     private int cerdos;
     private int pescado;
     private int edad;
-    private Persona Persona;//el nombre de esta clase puede cambiar dependiendo como le pongan  //agrege el nombre de la clase 
+   // private Persona Persona;//el nombre de esta clase puede cambiar dependiendo como le pongan  //agrege el nombre de la clase 
 
 //*****************************************************************************
 //Constructores
@@ -26,20 +26,51 @@ public class Organico {
 //*****************************************************************************
 //Metodos
 //*****************************************************************************
-    public int conversionFrutasVerduras(int frutasVerduras){
+    public int conversionFrutasVerduras(int frutasVerduras, int tiempo){
+        /**
+         * toma el valor de la cantidad de frutas o verduras que consume la 
+         * persona a la semana y lo multiplica por su edad o por alguna 
+         * cantidad de tiempo en específico
+         */
 
+        int conversion = frutasVerduras * tiempo;
+        return conversion;
     }
-    public int conversionPollo(int pollo){
-
+    public int conversionPollo(int pollo, int tiempo){
+        /**
+         *  toma el valor de la cantidad de libras de pollo que consume la 
+         * persona a la semana y lo multiplica por su edad o por alguna 
+         * cantidad de tiempo en específico
+         */
+        int conversion = pollo * tiempo;
+        return conversion;
     }
-    public int conversionCarne(int carne){
-
+    public int conversionCarne(int carne, int tiempo){
+        /**
+         *  toma el valor de la cantidad de libras de carne que consume 
+         * la persona a la semana y lo multiplica por su edad o por 
+         * alguna cantidad de tiempo en específico
+         */
+        int conversion = carne * tiempo;
+        return conversion;
     }
-    public int conversionCerdos(int cerdos){
-
+    public int conversionCerdos(int cerdos, int tiempo){
+        /**
+         * toma el valor de la cantidad de libras de cerdo que 
+         * consume la persona a la semana y lo multiplica por su 
+         * edad o por alguna cantidad de tiempo en específico
+         */
+        int conversion = cerdos * tiempo;
+        return conversion;
     }
-    public int conversionPescados(int pescado){
-        //coment
+    public int conversionPescados(int pescado, int tiempo){
+        /**
+         * toma el valor de la cantidad de libras de pescado que 
+         * consume la persona a la semana y lo multiplica por su 
+         * edad o por alguna cantidad de tiempo en específico
+         */
+        int conversion = pescado * tiempo;
+        return conversion;
     }
 
 
