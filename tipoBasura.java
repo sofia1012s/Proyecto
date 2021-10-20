ba3b0c75cf633c7e741c15e0a78aa884cc678a19
Sofía Salguero <salguerosofia.1012@gmail.com>
2021-10-20 12:03:07 -0600

    Se cambiaron los tipos de clases

--- tipoBasura.java
+++ tipoBasura.java
@@ -1,3 +1,10 @@
 public abstract class tipoBasura {
     
+    protected int frutasVerduras;
+    protected int pollo;
+    protected int carne;
+    protected int cerdos;
+    protected int pescado;
+    protected int cuatroSemanas = 4;
+
 }
