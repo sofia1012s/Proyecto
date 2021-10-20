e59c9dd5c843f36593c528ecec2db7f8f124d761
Sofía Salguero <salguerosofia.1012@gmail.com>
2021-10-20 12:20:32 -0600

    Update Controlador.java

--- Controlador.java
+++ Controlador.java
@@ -1,221 +1,218 @@
 import java.util.*;
-public class Controlador{
-	
-	public static void main(String[] args){
+
+public class Controlador {
+
+	public static void main(String[] args) {
 		Vista vis = new Vista();
 		PersonaPromedio pp = new PersonaPromedio();
 		Organico org = new Organico();
 		Inorganico ing = new Inorganico();
 		Persona per = new Persona();
 		Comparaciones com = new Comparaciones();
-		
+		tipoBasura basura = new tipoBasura();
+
 		vis.Saludo();
 		per.setNombre(vis.Nombre());
 		int opcion = 0;
 		boolean p = false;
-		do{
-			try{
+		do {
+			try {
 				vis = new Vista();
 				per.setEdad(vis.Edad());
 				p = true;
-			}
-			catch(InputMismatchException ex){
+			} catch (InputMismatchException ex) {
 				System.out.println("\nPor favor, ingrese un numero.\n");
 				p = false;
-			}
-			catch(Exception e){
+			} catch (Exception e) {
 				System.out.println("\nHubo un error, por favor intente otra opcion.\n");
 				p = false;
 			}
-		}while(!p);
+		} while (!p);
 		vis.Division(40);
-		while(opcion !=5){
-			do{
-				try{
+		while (opcion != 5) {
+			do {
+				try {
 					vis = new Vista();
 					opcion = vis.Menu(opcion);
 					p = true;
-				}
-				catch(InputMismatchException ex){
+				} catch (InputMismatchException ex) {
 					vis.Division(40);
 					System.out.println("\nPor favor, ingrsar un numero\n");
 					p = false;
 				}
-			}while(!p);
-			switch(opcion){
-				case 1:
-				do{
-					try{
+			} while (!p);
+			switch (opcion) {
+			case 1:
+				do {
+					try {
 						vis = new Vista();
 						vis.Division(40);
 						org.setFV(vis.FV());
 						p = true;
-					}
-					catch(InputMismatchException ex){
+					} catch (InputMismatchException ex) {
 						System.out.println("\nPor favor, ingrese un numero.\n");
 						p = false;
-					}
-					catch(Exception e){
+					} catch (Exception e) {
 						System.out.println("\nHubo un error, por favor intente de nuevo.\n");
 						p = false;
 					}
-				}while(!p);
-				do{
-					try{
+				} while (!p);
+				do {
+					try {
 						vis = new Vista();
 						vis.Division(40);
 						org.setPollo(vis.Pollo());
 						p = true;
-					}
-					catch(InputMismatchException ex){
+					} catch (InputMismatchException ex) {
 						System.out.println("\nPor favor, ingrese un numero.\n");
 						p = false;
-					}
-					catch(Exception e){
+					} catch (Exception e) {
 						System.out.println("\nHubo un error, por favor intente de nuevo.\n");
 						p = false;
 					}
-				}while(!p);
-				do{
-					try{
+				} while (!p);
+				do {
+					try {
 						vis = new Vista();
 						vis.Division(40);
 						org.setCerdo(vis.Cerdo());
 						p = true;
-					}
-					catch(InputMismatchException ex){
+					} catch (InputMismatchException ex) {
 						System.out.println("\nPor favor, ingrese un numero. \n");
 						p = false;
-					}
-					catch(Exception e){
+					} catch (Exception e) {
 						System.out.println("\nHubo un error, por favor intente de nuevo.\n");
 						p = false;
 					}
-				} while(!p);
-				do{
-					try{
+				} while (!p);
+				do {
+					try {
 						vis = new Vista();
 						vis.Division(40);
 						org.setPescado(vis.Pescado());
 						vis.Division(40);
 						p = true;
-					}
-					catch(InputMismatchException ex){
+					} catch (InputMismatchException ex) {
 						System.out.println("\nPor favor, ingrese un numero. \n");
 						p = false;
-					}
-					catch(Exception e){
+					} catch (Exception e) {
 						System.out.println("\nHubo un error, por favor intente de nuevo.\n");
 						p = false;
 					}
-				} while(!p);
+				} while (!p);
 				break;
-				case 2:
-				do{
-					try{
+			case 2:
+				do {
+					try {
 						vis = new Vista();
 						vis.Division(40);
 						ing.setBotellas(vis.Botellas());
 						p = true;
-					}
-					catch(InputMismatchException ex){
+					} catch (InputMismatchException ex) {
 						System.out.println("\nPor favor, ingrese un numero. \n");
 						p = false;
-					}
-					catch(Exception e){
+					} catch (Exception e) {
 						System.out.println("\nHubo un error, por favor intente de nuevo.");
 						p = false;
 					}
-				} while(!p);
-				do{
-					try{
+				} while (!p);
+				do {
+					try {
 						vis = new Vista();
 						vis.Division(40);
 						ing.setPapel(vis.Papel());
 						p = true;
-					}
-					catch(InputMismatchException ex){
+					} catch (InputMismatchException ex) {
 						System.out.println("\nPor favor, ingrese un numero. \n");
 						p = false;
-					}
-					catch(Exception e){
+					} catch (Exception e) {
 						System.out.println("\nHubo un error, por favor intente de nuevo.");
 						p = false;
 					}
-				} while(!p);
-				do{
-					try{
+				} while (!p);
+				do {
+					try {
 						vis = new Vista();
 						vis.Division(40);
 						ing.setTiempo(vis.Tiempo());
 						p = true;
-					}
-					catch(InputMismatchException ex){
+					} catch (InputMismatchException ex) {
 						System.out.println("\nPor favor, ingrese un numero. \n");
 						p = false;
-					}
-					catch(Exception e){
+					} catch (Exception e) {
 						System.out.println("\nHubo un error, por favor intente de nuevo.");
 						p = false;
 					}
-				} while(!p);
-				do{
-					try{
+				} while (!p);
+				do {
+					try {
 						vis = new Vista();
 						vis.Division(40);
 						ing.setBolsas(vis.Bolsas());
 						vis.Division(40);
 						p = true;
-					}
-					catch(InputMismatchException ex){
+					} catch (InputMismatchException ex) {
 						System.out.println("\nPor favor, ingrese un numero. \n");
 						p = false;
-					}
-					catch(Exception e){
+					} catch (Exception e) {
 						System.out.println("\nHubo un error, por favor intente de nuevo.");
 						p = false;
 					}
-				} while(!p);
+				} while (!p);
 				break;
-				case 3:
-				do{
-					try{
+			case 3:
+				do {
+					try {
 						com.comparacionBotellas(ing.getBotellas());
 						com.comparacionPapel(ing.getPapel());
 						com.comparacionAgua(ing.getTiempo());
 						com.comparacionBolsas(ing.getBolsas());
 						vis.Division(40);
 						System.out.println("\nResultados: \n");
-						vis.Resultados("\nSi toda la poblacion de Guatemala (16,600,000) gastara la misma cantidad de botellas por semana, se llenarian: ",com.comparacionBotellas(ing.getBotellas())," canchas de futbol cada semana.");
-						vis.Resultados("\nCon la cantidad de hojas que gasta por semana, se tardaría: ",com.comparacionPapel(ing.getPapel())," semanas en imprimir el libro Quijote de la Mancha");
-						vis.Resultados("\nEl tiempo que tardas bañandote podria llenar una piscina en: ",com.comparacionAgua(ing.getTiempo())," dias.");
-						vis.Resultados("\nSi toda la poblacion de Guatemala (16,600,000) gastara la misma cantidad de bolsas por semana, se llenarian: ",com.comparacionBolsas(ing.getBolsas())," canchas de baloncesto cada semana.");
+						vis.Resultados(
+								"\nSi toda la poblacion de Guatemala (16,600,000) gastara la misma cantidad de botellas por semana, se llenarian: ",
+								com.comparacionBotellas(ing.getBotellas()), " canchas de futbol cada semana.");
+						vis.Resultados("\nCon la cantidad de hojas que gasta por semana, se tardaría: ",
+								com.comparacionPapel(ing.getPapel()),
+								" semanas en imprimir el libro Quijote de la Mancha");
+						vis.Resultados("\nEl tiempo que tardas bañandote podria llenar una piscina en: ",
+								com.comparacionAgua(ing.getTiempo()), " dias.");
+						vis.Resultados(
+								"\nSi toda la poblacion de Guatemala (16,600,000) gastara la misma cantidad de bolsas por semana, se llenarian: ",
+								com.comparacionBolsas(ing.getBolsas()), " canchas de baloncesto cada semana.");
 						vis.Division(2);
 						int[] A;
-						String[] B = {"agua","botellas","papel","bolsas","frutas y verduras","pollo","carne","cerdo","pescado"};
-						A = com.comparacionPersonaP(pp.getAguaPromedio(),ing.conversionAgua(),pp.getBotellasPromedio(),ing.conversionBotellas(),pp.getPapelPromedio(),ing.conversionPapel(),pp.getBolsasPromedio(),ing.conversionBolsas(),pp.getFrutasVerdurasPromedio(),org.conversionFrutasVerduras(),pp.getPolloPromedio(),org.conversionPollo(),pp.getCarnePromedio(),org.conversionCarne(),pp.getCerdoPromedio(),org.conversionCerdos(),pp.getPescadossPromedio(),org.conversionPescados());
-						for(int i = 0; i < A.length; i++){
-							if(A[i] == 1){
-								System.out.println("Su consumo de " + B[i] + " es mayor que el de la persona promedio.\n");
-							}
-							else{
-								System.out.println("Su consumo de " + B[i] + " esta por debajo del de la persona promedio.\n");
+						String[] B = { "agua", "botellas", "papel", "bolsas", "frutas y verduras", "pollo", "carne",
+								"cerdo", "pescado" };
+						A = com.comparacionPersonaP(pp.getAguaPromedio(), ing.conversionAgua(),
+								pp.getBotellasPromedio(), basura.conversionMes(ing.getBotellas()), pp.getPapelPromedio(),
+								basura.conversionMes(ing.getPapel()), pp.getBolsasPromedio(), basura.conversionMes(ing.getBolsas()),
+								pp.getFrutasVerdurasPromedio(), basura.conversionMes(org.getFrutasVerduras()), pp.getPolloPromedio(),
+								basura.conversionMes(org.getPollo()), pp.getCarnePromedio(), basura.conversionMes(org.getCarne()),
+								pp.getCerdoPromedio(), basura.conversionMes(org.getCerdo()), pp.getPescadossPromedio(),
+								basura.conversionMes(org.getCerdo()));
+						for (int i = 0; i < A.length; i++) {
+							if (A[i] == 1) {
+								System.out.println(
+										"Su consumo de " + B[i] + " es mayor que el de la persona promedio.\n");
+							} else {
+								System.out.println(
+										"Su consumo de " + B[i] + " esta por debajo del de la persona promedio.\n");
 							}
 						}
 						p = true;
-					}
-					catch(ArithmeticException ex){
+					} catch (ArithmeticException ex) {
 						vis.Division(40);
-						System.out.println("\nRecordatorio: \n \nSi desea obtener los resultados por favor, ingrese los consumos organicos e inorganicos.\n");
+						System.out.println(
+								"\nRecordatorio: \n \nSi desea obtener los resultados por favor, ingrese los consumos organicos e inorganicos.\n");
 						break;
-					}
-					catch(Exception e){
+					} catch (Exception e) {
 						System.out.println("\nHubo un error, intente de nuevo.\n");
 						break;
 					}
-				}while(!p);
+				} while (!p);
 				break;
-				case 4:
+			case 4:
 				vis.Division(40);
 				System.out.println("\nRecomendaciones: \n");
 				vis.Recomendaciones();
