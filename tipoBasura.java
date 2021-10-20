9a222f305a9b4be2709c5da32c4e66159655599c
Sofía Salguero <salguerosofia.1012@gmail.com>
2021-10-20 12:22:58 -0600

    Se quitaron variables repetidas

--- tipoBasura.java
+++ tipoBasura.java
@@ -1,11 +1,15 @@
 public class tipoBasura {
-    
     protected int frutasVerduras;
     protected int pollo;
     protected int carne;
     protected int cerdos;
     protected int pescado;
     protected int cuatroSemanas = 4;
+
+    protected int botellas;
+    protected int papel;
+    protected int tiempo;
+    protected int bolsas;
 
     public int conversionMes(int varible)
     {
